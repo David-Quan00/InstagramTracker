b1482dda7f9b9a36506b35441c06f09787b69c04
David Quan <43793121+David-Quan00@users.noreply.github.com>
2023-10-05 14:30:28 -0400

    Update instagramTracker_main.py

--- instagramTracker_main.py
+++ instagramTracker_main.py
@@ -27,6 +27,7 @@ for i in data['relationships_followers']:
 
 print("List of my Follwoers:")
 print(lst_followers)
+print()
 
 # Closing file
 f.close()
@@ -41,28 +42,29 @@ for i in data['relationships_following']:
     for j in i["string_list_data"]:
         lst_following.append(j["value"])
 print(lst_following)
+print()
 # Closing file
 f.close()
 
 # Loose verification to see if my code worked
+print("Number of followers:")
 print(len(lst_followers))
+print("Number following:")
 print(len(lst_following))
+print()
 # Checks out with my current followers vs following count
 
 # Create a master list containing my followers AND following
 both_lst = []
 both_lst = lst_followers + lst_following
-print(both_lst)
+# print(both_lst)
 
 # Iterate through and print the accounts that I follow, but dont follow me.
 # Really this is either one of two cases:
 # (1) I follow and they don't follow me back OR (2) They follow me and I don't follow back (which I don't unfollow people)
-print("The individuals that either: Only I follow (they dont follow me back), or they follow me (and I dont follow back)")
+print("The individuals that either: Only I follow (they dont follow me back), or they follow me (and I dont follow back):")
 # Compare:
 for element in both_lst:
     if element not in lst_followers and element in lst_following:
         print(element)
         
-
-
-
